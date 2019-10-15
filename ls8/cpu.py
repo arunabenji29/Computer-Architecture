df3943d2a1e2e8798c2a0b1b3af9850c7cc7ffe7
Aruna Benjamin <arunabenji29@gmail.com>
2019-10-15 17:33:28 -0600

    it multiplies

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,26 +19,50 @@ class CPU:
         MDR = value
         self.ram[address] = value
 
-    def load(self):
+    def load(self,filename):
         """Load a program into memory."""
 
-        address = 0
+        try:
+
+            address = 0
+
+            with open(filename) as f:
+                for line in f:
+                    # print(f'\n\n{line}')
+                    hash_split = line.split("#")
+                    # print(f'hash split: {hash_split}')    
+                    binary_code = hash_split[0].strip()
+                    # print(f'binary code {type(binary_code)}')
+                    try:
+                        if binary_code is not None:
+                            # val = int(f'0b{binary_code}',2)
+                            val = int(binary_code)
+                    except ValueError:
+                        continue
+                    # print(f'val: {val}')
+                    self.ram_write(address,val)
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]} : {sys.argv[1]} not found")
+            sys.exit(1)
+        # print(f'Ram contents: {self.ram}')
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -46,7 +70,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -77,25 +102,34 @@ class CPU:
         operand_b = self.ram_read(self.pc+2)
 
         running = True
-        self.load()
-        # print(self.ram_read(0)) 
         curr_addr = 0
-        
+
         while running:
-            if self.ram_read(curr_addr) == 130 :
+
+            if int(f'{self.ram_read(curr_addr)}',2) == 130 :
                 self.reg[self.ram_read(curr_addr+1)] = self.ram_read(curr_addr+2)
-                print(f'Register value {self.reg[self.ram_read(curr_addr+1)]}')
-                # print(f'Register value {self.reg[self.ram_read(curr_addr+4)]}')
-                curr_addr += 3
-            elif self.ram_read(curr_addr) == 71:
-                print(f'address is : {curr_addr}')
+                # print(f'Register value {self.reg[self.ram_read(curr_addr+1)]}')
+                shift = int(f'{self.ram_read(curr_addr)}',2)
+                incr = shift >> 6
+                curr_addr += (incr + 1)
+
+            elif int(f'{self.ram_read(curr_addr)}',2) == 71:
                 print(self.reg[self.ram_read(curr_addr+1)])
-                curr_addr += 2
-            elif self.ram_read(curr_addr) == 1:
-                print(f'hlt : {self.ram_read(curr_addr)}')
+                shift = int(f'{self.ram_read(curr_addr)}',2)
+                incr = shift >> 6
+                curr_addr += (incr + 1)
+
+            elif int(f'{self.ram_read(curr_addr)}',2) == 162:
+                self.alu('MUL',self.ram_read(curr_addr+1),self.ram_read(curr_addr+2))
+                print(f"mul result: {self.reg[0]} conv to int: {int(f'{self.reg[0]}',2)}")
+                shift = int(f'{self.ram_read(curr_addr)}',2)
+                incr = shift >> 6
+                curr_addr += (incr + 1)
+
+            elif int(f'{self.ram_read(curr_addr)}',2) == 1:
                 sys.exit(1)
 
-
+        
 
 # cpu = CPU()
 
